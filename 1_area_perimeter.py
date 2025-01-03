9dc2f64d1cbd5e0d9f836903d247c7a06a1c899e
John William S <williamsanthosh575@gmail.com>
2025-01-03 14:26:37 +0530

    Add files via upload

---
+++ 1_area_perimeter.py
@@ -0,0 +1,5 @@
+r=int(input("Enter the radius of the circle"))
+pi=3.14
+area=pi*r**2
+perimetre=2*pi*r
+print(f"The area of the circle is {area}\nThe perimetre of the circle is {perimetre}")
---
+++ 2_swap.py
@@ -0,0 +1,4 @@
+a=int(input("enter value of a :"))
+b=int(input("enter value of b:"))
+a,b=b,a
+print("the value of a is ",a,"\n the value of b is",b)
