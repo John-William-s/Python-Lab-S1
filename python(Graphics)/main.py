0ed69a5e689b277255db39fdb594f003f9378b4b
John William Santhosh <williamsanthosh575@gmail.com>
2025-01-06 22:43:06 +0530

    Graphics

---
+++ python(Graphics)/main.py
@@ -0,0 +1,8 @@
+from graphics import rectangle,circle
+
+from graphics.dgraphics import sphere,cuboid
+
+print(rectangle.area(5,6))
+print(circle.area(5))
+print(sphere.area(5))
+print(cuboid.area(5,4,2))
